1a5e4eb0ea77419ca3a0c6f16896e306f31b48b3
mymyeong <mymyeong@61.79.138.28>
2022-03-09 12:00:17 +0900

    user point paging 처리

--- musinsa/src/main/java/com/mymyeong/musinsa/userpoint/UserPointController.java
+++ musinsa/src/main/java/com/mymyeong/musinsa/userpoint/UserPointController.java
@@ -3,6 +3,7 @@ package com.mymyeong.musinsa.userpoint;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ public class UserPointController {
 	@Autowired
 	UserRepository userRepository;
 
+	@Autowired
 	UserPointRepository userPointRepository;
 
 	@GetMapping("/users")
@@ -22,13 +24,20 @@ public class UserPointController {
 		return userRepository.findAll();
 	}
 
-	@GetMapping("/users/{userId}")
-	public User retrieveAllUsers(@PathVariable Long userId) {
-		return userRepository.findById(userId).orElse(null);
+	@GetMapping("/users/{userNo}")
+	public User retrieveAllUsers(@PathVariable Long userNo) {
+		return userRepository.findById(userNo).orElse(null);
 	}
 
-	@GetMapping("/users/{userId}/point")
-	public List<UserPoint> retrieveUserPoint(@PathVariable Long userId) {
-		return null;
+	@GetMapping("/users/{userNo}/point")
+	public List<UserPoint> retrieveUserPoint(@PathVariable Long userNo, Pageable pageable) throws Exception {
+		User user = userRepository.findById(userNo).orElse(null);
+
+		if (user == null) {
+			throw new Exception("존재하지 않는 사용자 NO 입니다.");
+		}
+
+		return userPointRepository.findByUserOrderByEarnDateDesc(user, pageable).toList();
 	}
+
 }
--- musinsa/src/main/java/com/mymyeong/musinsa/userpoint/UserPointRepository.java
+++ musinsa/src/main/java/com/mymyeong/musinsa/userpoint/UserPointRepository.java
@@ -1,9 +1,11 @@
 package com.mymyeong.musinsa.userpoint;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserPointRepository extends JpaRepository<UserPoint, Integer> {
-
+public interface UserPointRepository extends JpaRepository<UserPoint, Long> {
+	Page<UserPoint> findByUserOrderByEarnDateDesc(User user, Pageable pageable);
 }

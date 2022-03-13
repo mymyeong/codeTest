ac506840a7a9d4a85e1e7b454adee31fb9728b09
mymyeong <mymyeong@61.79.138.28>
2022-03-14 00:02:47 +0900

    ExceptionHandler 주석처리 제거

--- src/main/java/com/mymyeong/codetest/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/mymyeong/codetest/exception/CustomizedResponseEntityExceptionHandler.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-//@RestController
-//@ControllerAdvice
+@RestController
+@ControllerAdvice
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(Exception.class)
